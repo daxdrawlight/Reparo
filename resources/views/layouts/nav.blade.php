0217a45eca34b749aaee9ab129e93565dc6b5c11
Deni Pešić <pesic.deni@gmail.com>
2017-08-31 15:52:54 +0200

    Sidebar placeholders

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -10,6 +10,15 @@
                 @include ('layouts.partials.nav_items')
               </ul>
               <ul class="side-nav" id="mobile-demo">
+              <li><div class="user-view">
+                <div class="background">
+                  <img src="images/laptop.jpeg">
+                </div>
+                  <a href="#!user"><img class="circle" src="images/denko.jpeg"></a>
+                  <a href="#!name"><span class="white-text name">John Doe</span></a>
+                  <a href="#!email"><span class="white-text email">johndoe@email.com</span></a>
+                </div>
+              </li>
                 @include ('layouts.partials.nav_items')
               </ul>
             </div>
